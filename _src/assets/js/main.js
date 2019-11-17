f8297396892dd18be933fb22906956c3ae8a641c
María García de Lomana <mariagarciadelomana@gmail.com>
2019-11-17 20:38:34 +0100

    beginning js structure

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-console.log('>> Ready :)');
+//const selectedSection = document.querySelector('.');--> clase de la sección q aparece por defecto
+//const selectedPalette = document.querySelector('.');--> clase de la paleta q aparece por defecto
+
+const name = document.querySelector('#name');
+const job = document.querySelector('#job-title');
+const email = document.querySelector('#email');
+const tel = document.querySelector('#tel');
+const linkedin = document.querySelector('#linkedin');
+const github = document.querySelector('#github');
+
+// start page functions
+
+const init = () => {
+    displaySection();
+    console.log('Aparece la primera sección abierta y las otras cerradas');
+    applyPalette();
+    console.log('Se aplica la paleta de colores por defecto');
+
+};
+
+// dom listeners
+
+const applyPalette = () => {
+    console.log('Se aplica la paleta de colores elegida');
+};
+
+const displaySection = () => {
+    console.log('Se abre la sección seleccionada y se cierran el resto');
+};
+
+const handleInput = () => {
+
+};
+
+// functions
+
+
+
+// listeners
+
+selectedPalette.addEventListener('click', applyPalette);
+selectedSection.addEventListener('click', displaySection);
+name.addEventListener('keyup', handleInput);
+job.addEventListener('keyup', handleInput);
+email.addEventListener('keyup', handleInput);
+tel.addEventListener('keyup', handleInput);
+linkedin.addEventListener('keyup', handleInput);
+github.addEventListener('keyup', handleInput);
+
+// init page
+
+init();
