3f42df94327d5f38411a9e4e044e8844a3186437
mariaadobos <mariyadob@gmail.com>
2019-11-27 00:28:02 +0100

    review 02-palette

--- _src/assets/js/02-palette.js
+++ _src/assets/js/02-palette.js
@@ -1,20 +1,15 @@
 'use strict';
 
-
 //COLORS
-
 
 function applyPalette1() {
     previewCardName.classList.add('darkGreenBlue');
-    previewCardName.classList.remove('driedBlood');
-    previewCardName.classList.remove('slate');
+    previewCardName.classList.remove('driedBlood', 'slate');
     previewCardHead.classList.add('borderPalette1');
-    previewCardHead.classList.remove('borderPalette2');
-    previewCardHead.classList.remove('borderPalette3');
-    for (const IconItem of IconItems) {
+    previewCardHead.classList.remove('borderPalette2', 'borderPalette3');
+    for (const iconItem of iconItems) {
         iconItem.classList.add('darkGreenBlue');
-        iconItem.classList.remove('driedBlood');
-        iconItem.classList.remove('slate');
+        iconItem.classList.remove('driedBlood', 'slate');
     }
     for (const IconButton of iconButtons) {
         IconButton.style.borderColor = '#a2deaf';
@@ -25,16 +20,12 @@ palette1.addEventListener('click', applyPalette1);
 
 function applyPalette2() {
     previewCardName.classList.add('driedBlood');
-    previewCardName.classList.remove('darkGreenBlue');
-    previewCardName.classList.remove('slate');
-
+    previewCardName.classList.remove('darkGreenBlue', 'slate');
     previewCardHead.classList.add('borderPalette2');
-    previewCardHead.classList.remove('borderPalette1');
-    previewCardHead.classList.remove('borderPalette3');
+    previewCardHead.classList.remove('borderPalette1', 'borderPalette3');
     for (const iconItem of iconItems) {
         iconItem.classList.add('driedBlood');
-        iconItem.classList.remove('darkGreenBlue');
-        iconItem.classList.remove('slate');
+        iconItem.classList.remove('darkGreenBlue', 'slate');
     }
     for (const IconButton of iconButtons) {
         IconButton.style.borderColor = '#e95626';
@@ -45,16 +36,12 @@ palette2.addEventListener('click', applyPalette2);
 
 function applyPalette3() {
     previewCardName.classList.add('slate');
-    previewCardName.classList.remove('driedBlood');
-    previewCardName.classList.remove('darkGreenBlue');
-
+    previewCardName.classList.remove('driedBlood', 'darkGreenBlue');
     previewCardHead.classList.add('borderPalette3');
-    previewCardHead.classList.remove('borderPalette1');
-    previewCardHead.classList.remove('borderPalette2');
+    previewCardHead.classList.remove('borderPalette1', 'borderPalette2');
     for (const iconItem of iconItems) {
         iconItem.classList.add('slate');
-        iconItem.classList.remove('driedBlood');
-        iconItem.classList.remove('darkGreenBlue');
+        iconItem.classList.remove('driedBlood', 'darkGreenBlue');
     }
     for (const IconButton of iconButtons) {
         IconButton.style.borderColor = '#a0c0cf';
