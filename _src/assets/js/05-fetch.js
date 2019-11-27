a3904b6b0c4a426b1ba763d1b540ef11410d7155
mariaadobos <mariyadob@gmail.com>
2019-11-27 23:22:03 +0100

    add twitter button

--- _src/assets/js/05-fetch.js
+++ _src/assets/js/05-fetch.js
@@ -1,10 +1,10 @@
 'use strict'
 
 function sendForm (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-//creo json
- const datos = {
+  //creo json
+  const datos = {
     "palette": paletteChosen,
     "name": userName.value,
     "job": userJob.value,
@@ -13,30 +13,40 @@ function sendForm (event) {
     "linkedin": userLinkedin.value,
     "github": userGithub.value,
     "photo": fr.result,
- };
- //petición
- fetch ('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-     method: 'POST',
-     body: JSON.stringify(datos),
-     headers: {
-         'content-type': 'application/json'
-     },
- })
- .then (response => response.json())
- .then (data => showURL(data))
- .catch(function(error) {console.log(error);});
+  };
+  //petición
+  fetch ('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then (response => response.json())
+    .then (data => showURL(data))
+    .catch(function(error) {console.log(error);});
+}
+function showTwitterSection (){
+  twitterContainer.classList.remove('hidden');
+}
+function shareTwitter(url){
+  const twitterText = encodeURIComponent('He creado esta tarjeta con Awesome Profile Cards. ¡Échale un vastazo!');
+  const twitterHashtag = encodeURIComponent('adalab,adalaber,frontEnd,awesomeCards');
+  const twitterLink = `https://twitter.com/intent/tweet?text=${url}`;
+  twitterBtn.href = `https://twitter.com/intent/tweet?text=${twitterText}&url=${url}&hashtags=${twitterHashtag}`;
 }
 function showURL (data) {
-    const twitterURL = document.createElement('span');
-   if (data.success) {
+  const twitterURL = document.createElement('span');
+  if (data.success) {
     twitterURL.innerHTML = `<a class="twitter-url" href=${data.cardURL} target="_blank">${data.cardURL}</a>`;
+    console.log(data.cardURL)
     twitterMessage.insertAdjacentElement('afterend', twitterURL);
     shareTwitter(data.cardURL);
-    console.log(data.cardURL)
-    //showTwitterSection();
+    showTwitterSection();
     //showTwitter();
-} else {
+  } else {
     twitterURL.innerHTML = 'ERROR' + data.error;
+  }
 }
-}
+
 createCardButton.addEventListener('click', sendForm);
